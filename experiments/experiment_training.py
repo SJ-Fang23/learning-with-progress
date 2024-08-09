82e6b7bf2ae8a885003475f6934aeb36eb08aa36
Frank F <fangshijie2001@hotmail.com>
2024-08-08 21:47:14 -0400

    added new loss, add ent_coef

--- experiments/experiment_training.py
+++ experiments/experiment_training.py
@@ -8,6 +8,7 @@ from stable_baselines3.ppo import MlpPolicy
 from IRL_lib_mod.airl import AIRL
 from imitation.data import rollout
 from imitation.data.wrappers import RolloutInfoWrapper
+from envs.wrappers import SequentialObservationWrapper
 from imitation.policies.serialize import load_policy
 from imitation.rewards.reward_nets import BasicShapedRewardNet
 from imitation.util.networks import RunningNorm
@@ -21,17 +22,30 @@ from utils.demostration_utils import load_dataset_and_annotations_simutanously
 from utils.annotation_utils import read_all_json
 from imitation.util import logger as imit_logger
 import imitation.scripts.train_adversarial as train_adversarial
+import torch
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str, default="default_experiment")
-    
+    parser.add_argument('--continue_training', type=bool, default=False)
+    parser.add_argument('--checkpoint', type=str, default="320")
+    parser.add_argument('--load_exp_name', type=str, default="mh_sign_scale_loss_8m_1")
+    parser.add_argument('-s', '--sequence_keys', nargs='+', default=[])
+    parser.add_argument('-l', '--obs_seq_len', type=int, default=1)
+
+
     args = parser.parse_args()
+    print("sequence keys", args.sequence_keys)
+    print("obs_seq_len", args.obs_seq_len)
+
+
     project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     dataset_path = os.path.join(project_path,"human-demo/can-pick/low_dim_v141.hdf5")
+    
     log_dir = os.path.join(project_path,f"logs/{args.exp_name}")
     print(dataset_path)
+    
     f = h5py.File(dataset_path,'r')
 
     config_path = os.path.join(project_path,"configs/osc_position.json")
@@ -40,7 +54,7 @@ if __name__ == "__main__":
 
     controller_config = load_controller_config(default_controller="OSC_POSE")
     env_meta = json.loads(f["data"].attrs["env_args"])
-    SEED = 42
+    SEED = 2024
     make_env_kwargs = dict(
         robots="Panda",             # load a Sawyer robot and a Panda robot
         gripper_types="default",                # use default grippers per robot arm
@@ -54,6 +68,24 @@ if __name__ == "__main__":
         use_camera_obs=False,
         reward_shaping=True,
     )
+
+    print("sequence keys", args.sequence_keys)
+    if len(args.sequence_keys) > 0:
+        sequential_wrapper_kwargs = dict(
+            sequential_observation_keys = args.sequence_keys, 
+            sequential_observation_length = args.obs_seq_len, 
+            use_half_gripper_obs = True
+        )
+
+        seqential_wrapper_cls = SequentialObservationWrapper
+        make_sequential_obs = True
+
+
+    else:
+        sequential_wrapper_kwargs = None
+        seqential_wrapper_cls = None
+        make_sequential_obs = False
+
     envs = make_vec_env_robosuite(
         "PickPlaceCanModified",
         obs_keys = ["object-state","robot0_eef_pos", "robot0_eef_quat", "robot0_gripper_qpos"],
@@ -62,29 +94,48 @@ if __name__ == "__main__":
         parallel=True,
         post_wrappers=[lambda env, _: RolloutInfoWrapper(env)],  # to compute rollouts
         env_make_kwargs=make_env_kwargs,
-
+        sequential_wrapper = seqential_wrapper_cls,
+        sequential_wrapper_kwargs = sequential_wrapper_kwargs
     )
 
     annotation_dict = read_all_json("progress_data")
 
-    trajs = load_dataset_to_trajectories(["object","robot0_eef_pos", "robot0_eef_quat", "robot0_gripper_qpos"])
+    trajs = load_dataset_to_trajectories(["object","robot0_eef_pos", "robot0_eef_quat", "robot0_gripper_qpos"],
+                                         dataset_path = "human-demo/can-pick/can_low_dim_mh.hdf5", 
+                                            make_sequential_obs=make_sequential_obs,
+                                         sequential_obs_keys=args.sequence_keys,
+                                         obs_seq_len=args.obs_seq_len,
+                                         use_half_gripper_obs=True)
+    
+    for i in range(len(trajs)):
+        if trajs[i].obs.shape[1] != 21 + args.obs_seq_len:
+            print(trajs[i].obs.shape)
+
     trajs_for_shaping, annotation_list = load_dataset_and_annotations_simutanously(["object","robot0_eef_pos", "robot0_eef_quat", "robot0_gripper_qpos"],
                                                                        annotation_dict=annotation_dict,
-                                                                       dataset_path=dataset_path)
+                                                                       dataset_path=dataset_path,
+                                                                       make_sequential_obs=make_sequential_obs,
+                                         sequential_obs_keys=args.sequence_keys,
+                                         obs_seq_len=args.obs_seq_len,
+                                         use_half_gripper_obs=True)
     # type of reward shaping to use
     # change this to enable or disable reward shaping
-    shape_reward = []
-                                                                       
+    shape_reward = ["progress_sign_loss", "delta_progress_scale_loss", "progress_value_loss"]
+
+    for i in range(len(trajs_for_shaping)):
+        if trajs_for_shaping[i].obs.shape[1] != 21 + args.obs_seq_len:
+            print(i) 
+                                                            
     learner = PPO(
         env=envs,
         policy=MlpPolicy,
         batch_size=1024,
-        ent_coef=0.0,
+        ent_coef=1.0,
         learning_rate=0.0005,
         gamma=0.95,
-        clip_range=0.1,
-        vf_coef=0.1,
-        n_epochs=5,
+        clip_range=0.3,
+        vf_coef=0.5,
+        n_epochs=10,
         seed=SEED,
     )
     reward_net = BasicShapedRewardNet(
@@ -92,6 +143,13 @@ if __name__ == "__main__":
         action_space=envs.action_space,
         normalize_input_layer=RunningNorm,
     )
+
+    generator_model_path = f"{project_path}/checkpoints/{args.load_exp_name}/{args.checkpoint}/gen_policy/model"
+    if args.continue_training:
+        reward_net = (torch.load(f"{project_path}/checkpoints/{args.load_exp_name}/{args.checkpoint}/reward_train.pt"))
+        learner = PPO.load(generator_model_path)
+
+    
     # logger that write tensroborad to logs dir
     logger = imit_logger.configure(folder=log_dir, format_strs=["tensorboard"])
     airl_trainer = AIRL(
@@ -120,7 +178,7 @@ if __name__ == "__main__":
         learner, envs, 12, return_episode_rewards=True,
     )
     airl_trainer.train(8_000_000)  # Train for 2_000_000 steps to match expert.
-    envs.seed(SEED)
+    # envs.seed(SEED)
     learner_rewards_after_training, _ = evaluate_policy(
         learner, envs, 12, return_episode_rewards=True,
     )
